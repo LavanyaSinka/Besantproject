f4c8f3d8f3b3c2979219774c26056d312333eaf6
LavanyaSinka <sinkalavanya05@gmail.com>
2024-02-09 08:28:30 +0000

    datatypes file got added

---
+++ datatypes.java
@@ -0,0 +1,10 @@
+Class Demo{
+	public static void main(String[] args){
+		int a=30;
+		int b=40;
+		System.out.println(a+b);
+		String x="Hello";
+		String y="System";
+		System.out.println(a+b+x+y);
+	}
+}
